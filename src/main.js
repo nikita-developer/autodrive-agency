b4e19deac9795cc1c5d4931d9a5ffdf9a3af02bd
kuznetsov.n <nikita.kuznetsov.r@gmail.com>
2022-09-27 17:53:16 +0300

    start

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 import store from './store'
+// import axios from 'axios'
+import '@/assets/css/tailwind.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(store).mount('#app')
